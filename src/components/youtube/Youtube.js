315a28ffe0c6062ac55f75183ee15e275a14b0fe
plummoore <plummoore@gmail.com>
2017-12-11 19:33:47 +0000

    ability to add video to state

--- src/components/youtube/Youtube.js
+++ src/components/youtube/Youtube.js
@@ -4,19 +4,31 @@ import axios from 'axios';
 
 class Youtube extends React.Component {
   state = {
-    videos: []
+    videos: [],
+    savedVideos: []
   }
 
 
   componentDidMount() {
     axios
-    .get('https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCAuUUnT6oDeKwE6v1NGQxug&maxResults=16&order=viewCount&q=comedy&type=video&videoDuration=medium&videoEmbeddable=true&fields=items%2CpageInfo%2CprevPageToken%2CregionCode&key=AIzaSyAb3g7hxT7yujlkyViY5Knkk6aTTpRGRhQ')
+      .get('https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCAuUUnT6oDeKwE6v1NGQxug&maxResults=8&order=viewCount&q=all&type=video&videoDuration=medium&videoEmbeddable=true&fields=items%2CpageInfo%2CprevPageToken%2CregionCode&key=AIzaSyAb3g7hxT7yujlkyViY5Knkk6aTTpRGRhQ')
       .then(res => {
-        console.log(res.data.items);
+        // console.log(res.data.items);
         this.setState({videos: res.data.items});
-        console.log(this.state.videos);
+        // console.log(this.state.videos);
       })
       .catch(err => this.setState({ error: err.message}));
+  }
+
+  handleSave = (e) => {
+    console.log('this.state BEFORE', this.state);
+    const addedVideos = this.state.savedVideos.slice();
+    addedVideos.push(e);
+    this.setState({ savedVideos: addedVideos });
+    console.log('this.state AFTER',this.state);
+    // this.setstate({ savedVideos: e});
+    // console.log(this.state.savedVideos);
+
   }
 
   render() {
@@ -35,6 +47,7 @@ class Youtube extends React.Component {
                   frameBorder="0"
                   allowFullScreen>
                 </iframe>
+                <button onClick={() => this.handleSave(video.id.videoId)}><i className="fas fa-plus"></i></button>
                 {/* <p>{video.snippet.title}</p>
                 <p>{video.snippet.description}</p> */}
               </div>
